fd82a25bafe9430abe6ed851eadc83fc9f39f45d
Mohammed Rafi KC <rkavunga@redhat.com>
2018-08-18 01:25:34 +0530

    snapshot:Fail snapshot creation if an empty description provided
    Snapshot description should have a valid string. Creating a
    snapshot with null value will cause reading from info file
    to fail with a null exception
    
    >Change-Id: I9f84154b8e3e7ffefa5438807b3bb9b4e0d964ca
    >updates: bz#1618004
    >Signed-off-by: Mohammed Rafi KC <rkavunga@redhat.com>
    
    upstream patch: https://review.gluster.org/#/c/glusterfs/+/20770/
    
    Change-Id: I9f84154b8e3e7ffefa5438807b3bb9b4e0d964ca
    BUG: 1615578
    Signed-off-by: Sanju Rakonde <srakonde@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/147517
    Tested-by: RHGS Build Bot <nigelb@redhat.com>
    Reviewed-by: Atin Mukherjee <amukherj@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-snapshot.c
+++ xlators/mgmt/glusterd/src/glusterd-snapshot.c
@@ -2518,6 +2518,7 @@ glusterd_snapshot_create_prevalidate (dict_t *dict, char **op_errstr,
         int64_t                effective_max_limit = 0;
         int                    flags             = 0;
         uint64_t               opt_hard_max      = GLUSTERD_SNAPS_MAX_HARD_LIMIT;
+        char                  *description       = NULL;
 
         this = THIS;
         GF_ASSERT (op_errstr);
@@ -2541,6 +2542,15 @@ glusterd_snapshot_create_prevalidate (dict_t *dict, char **op_errstr,
         ret = dict_get_str (dict, "snapname", &snapname);
         if (ret) {
                 snprintf (err_str, sizeof (err_str), "Failed to get snapname");
+                goto out;
+        }
+
+        ret = dict_get_str (dict, "description", &description);
+        if (description && !(*description)) {
+                /* description should have a non-null value */
+                ret = -1;
+                snprintf (err_str, sizeof (err_str), "Snapshot cannot be "
+                          "created with empty description");
                 goto out;
         }
 
