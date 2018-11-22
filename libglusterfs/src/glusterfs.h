a357b89665212cdc976cf1f843fac4a0e8b81cd0
Atin Mukherjee <amukherj@redhat.com>
2018-11-22 09:58:52 +0530

    glusterd: perform store operation in cleanup lock
    All glusterd store operation and cleanup thread should work under a
    critical section to avoid any partial store write.
    
    > Change-Id: I4f12e738f597a1f925c87ea2f42565dcf9ecdb9d
    > Fixes: bz#1652430
    > Signed-off-by: Atin Mukherjee <amukherj@redhat.com>
    
    upstream patch: https://review.gluster.org/#/c/glusterfs/+/21702/
    
    Change-Id: I4f12e738f597a1f925c87ea2f42565dcf9ecdb9d
    BUG: 1654161
    Signed-off-by: Sanju Rakonde <srakonde@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/158804
    Tested-by: RHGS Build Bot <nigelb@redhat.com>
    Reviewed-by: Atin Mukherjee <amukherj@redhat.com>

--- libglusterfs/src/glusterfs.h
+++ libglusterfs/src/glusterfs.h
@@ -574,6 +574,7 @@ struct _glusterfs_ctx {
         char               btbuf[GF_BACKTRACE_LEN];
 
         pthread_mutex_t    notify_lock;
+        pthread_mutex_t    cleanup_lock;
         pthread_cond_t     notify_cond;
         int                notifying;
 
