8a72351bcac9694a4f90ffdf939012389ccbafea
Sanju Rakonde <srakonde@redhat.com>
2019-01-03 19:09:36 +0530

    glusterd: aggregate rsp from peers for profile command
    When we run profile info command, it should display statistics
    of all the bricks of the volume. To display information of bricks
    which are hosted on peers, we need to aggregate the response from
    peers.
    
    For profile info command, all the statistics will be added into
    the dictionary in brick-op phase. To aggregate the information from
    peers, we need to call glusterd_syncop_aggr_rsp_dict() in brick-op
    call back function.
    
    >upstream patch : https://review.gluster.org/#/c/glusterfs/+/21988
    >fixes: bz#1663223
    >Change-Id: I5f5890c3d01974747f829128ab74be6071f4aa30
    >Signed-off-by: Sanju Rakonde <srakonde@redhat.com>
    
    BUG: 1663232
    Change-Id: I5f5890c3d01974747f829128ab74be6071f4aa30
    Signed-off-by: Sanju Rakonde <srakonde@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/159818
    Tested-by: RHGS Build Bot <nigelb@redhat.com>
    Reviewed-by: Sunil Kumar Heggodu Gopala Acharya <sheggodu@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-mgmt.c
+++ xlators/mgmt/glusterd/src/glusterd-mgmt.c
@@ -1349,7 +1349,8 @@ gd_mgmt_v3_brick_op_cbk_fn (struct rpc_req *req, struct iovec *iov,
         gf_uuid_copy (args->uuid, rsp.uuid);
         pthread_mutex_lock (&args->lock_dict);
         {
-                if (rsp.op == GD_OP_DEFRAG_BRICK_VOLUME)
+                if (rsp.op == GD_OP_DEFRAG_BRICK_VOLUME ||
+                    rsp.op == GD_OP_PROFILE_VOLUME)
                         ret = glusterd_syncop_aggr_rsp_dict (rsp.op, args->dict,
                                                              rsp_dict);
         }
