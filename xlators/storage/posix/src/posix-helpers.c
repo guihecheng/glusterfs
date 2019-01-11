fc97697acfb4bb23f7af262ea13c6c53624b7a7c
Mohit Agrawal <moagrawa@redhat.com>
2019-01-11 11:57:20 +0530

    posix: posix_health_check_thread_proc crash due to priv is NULL
    Problem: posix_fini sends a cancellation request to health_check
             thread and cleanup priv without ensuring health_check thread
             is running
    
    Solution: Make health_check && disk_space thread joinable and call
              gf_thread_cleanup_xint to wait unless thread is not finished
    
    > Change-Id: I4d37b08138766881dab0922a47ed68a2c3411f13
    > fixes: bz#1636570
    > (Reviewed on upstream link https://review.gluster.org/#/c/glusterfs/+/21717/)
    > (Cherry pick from commit e82bcc33ed2d5cd54d3f918397f31818089299ad)
    
    Change-Id: I9edadd5bc445549b5f45bab98e4794d62a185f1c
    BUG: 1662828
    Signed-off-by: Mohit Agrawal <moagrawa@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/160404
    Tested-by: RHGS Build Bot <nigelb@redhat.com>
    Reviewed-by: Sunil Kumar Heggodu Gopala Acharya <sheggodu@redhat.com>

--- xlators/storage/posix/src/posix-helpers.c
+++ xlators/storage/posix/src/posix-helpers.c
@@ -1854,8 +1854,6 @@ posix_spawn_health_check_thread (xlator_t *xl)
                         goto unlock;
                 }
 
-                /* run the thread detached, resources will be freed on exit */
-                pthread_detach (priv->health_check);
                 priv->health_check_active = _gf_true;
         }
 unlock:
@@ -1958,9 +1956,9 @@ posix_spawn_disk_space_check_thread (xlator_t *xl)
                         priv->disk_space_check_active = _gf_false;
                 }
 
-                ret = gf_thread_create_detached (&priv->disk_space_check,
-                                                 posix_disk_space_check_thread_proc,
-                                                 xl, "posix_reserve");
+                ret = gf_thread_create (&priv->disk_space_check, NULL,
+                                        posix_disk_space_check_thread_proc,
+                                        xl, "posix_reserve");
                 if (ret < 0) {
                         priv->disk_space_check_active = _gf_false;
                         gf_msg (xl->name, GF_LOG_ERROR, errno,
